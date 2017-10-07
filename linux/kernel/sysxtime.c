42d12ba84c1abcea7f8b5f30d688fff02c23c61f
Zach H <zhalper3@binghamton.edu>
2017-10-06 23:32:14 -0400

    module update

--- linux/kernel/sysxtime.c
+++ linux/kernel/sysxtime.c
@@ -9,17 +9,24 @@
 asmlinkage int sys_my_xtime(struct timespec *current_time)
 {
 	struct timespec lCurrentTime = current_kernel_time();
+	int lCopy2UserStatus = 0;
+
+	printk(KERN_INFO "lCurrentTime = %ld", lCurrentTime.tv_sec);
 
 	// Check if pointer is a valid location to write to
 	if (! access_ok(VERIFY_WRITE, current_time, sizeof(current_time)))
 	{
+		printk(KERN_ERR "access_ok failed\n");
 		return -EFAULT;
 	}
 
 	// Attempt to copy value from xtime -> current_time
-	if (! copy_to_user(current_time, &lCurrentTime, sizeof(lCurrentTime)))
+	lCopy2UserStatus = copy_to_user(current_time, &lCurrentTime, sizeof(lCurrentTime));
+
+	if (lCopy2UserStatus > 0)
 	{
 		// If the copy failed for some reason
+		printk(KERN_ERR "copy_to_user failed: %d\n", lCopy2UserStatus);
 		return -EFAULT;
 	}
 

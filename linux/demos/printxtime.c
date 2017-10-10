5b69a00446e5e9e5b3ad0edcba3db7291cce8ee6
Zach H <zhalper3@binghamton.edu>
2017-10-10 12:05:11 -0400

    sys and print updates

--- linux/demos/printxtime.c
+++ linux/demos/printxtime.c
@@ -2,14 +2,19 @@
 #include <errno.h>
 #include <unistd.h>
 #include <linux/unistd.h>
+#include <time.h>
+
+#define __sys_x_time 326
 
 int main(void)
 {
-	int ret;
+	int lnReturnValue;
+	struct timespec loTimeSet;
+
 	while (1)
 	{
-		ret = syscall(326);
-		printf("ret=%d errno=%d\n", ret, errno);
+		ret = syscall(__sys_x_time, &loTimeSet);
+		printf("ret=%d errno=%d val=%d\n", lnReturnValue, errno, loTimeSet);
 		sleep(1);
 	}
 
--- linux/kernel/sysxtime.c
+++ linux/kernel/sysxtime.c
@@ -8,10 +8,8 @@
 
 asmlinkage int sys_my_xtime(struct timespec *current_time)
 {
-	struct timespec lCurrentTime = current_kernel_time();
-	int lCopy2UserStatus = 0;
-
-	printk(KERN_INFO "lCurrentTime = %ld", lCurrentTime.tv_sec);
+	struct timespec lCurrentTime;
+	int lCopy2UserStatus;
 
 	// Check if pointer is a valid location to write to
 	if (! access_ok(VERIFY_WRITE, current_time, sizeof(current_time)))
@@ -21,6 +19,7 @@ asmlinkage int sys_my_xtime(struct timespec *current_time)
 	}
 
 	// Attempt to copy value from xtime -> current_time
+	lCurrentTime = current_kernel_time();
 	lCopy2UserStatus = copy_to_user(current_time, &lCurrentTime, sizeof(lCurrentTime));
 
 	if (lCopy2UserStatus > 0)
@@ -30,7 +29,10 @@ asmlinkage int sys_my_xtime(struct timespec *current_time)
 		return -EFAULT;
 	}
 
-	printk(KERN_INFO "Current XTime: %ld\n", lCurrentTime.tv_sec*1000000000 + lCurrentTime.tv_nsec);
+	printk(KERN_INFO "current_kernel_time: %ld %ld\n", lCurrentTime.tv_sec, lCurrentTime.tv_nsec);
+	printk(KERN_INFO "current time in NS: %ld\n", lCurrentTime.tv_sec*1000000000UL + lCurrentTime.tv_nsec);
 
 	return 0;
 }
+
+EXPORT_SYMBOL(sys_my_xtime);

56860565acca80aa12509794e34e0e6a38ea7521
Zach H <zhalper3@binghamton.edu>
2017-10-21 17:06:16 -0400

    device driver

---
+++ linux/demos/module/mytime.c
@@ -0,0 +1,97 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/miscdevice.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/printk.h>
+#include <asm/uaccess.h>
+#include <linux/time.h>
+#include <linux/timekeeping.h>
+#include <linux/vmalloc.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Zach Halpern");
+MODULE_VERSION("0.0.2");
+
+static char *name = "World";
+module_param(name, charp, S_IRUGO);
+MODULE_PARM_DESC(name, "Name to display in dmesg");
+
+static ssize_t my_read(struct file *f, char __user *buffer, size_t length, loff_t *offset)
+{
+	int lnCopyToUser;
+	struct timespec lCurrentTimeKernel;
+	struct timespec lCurrentTimeDay;
+	char *lspOutBuffer = kmalloc(1, GFP_KERNEL);
+
+	if (lspOutBuffer)
+	{
+		printk(KERN_ERR "ENOMEM ISSUE");
+		return -ENOMEM;
+	}
+
+    if (! access_ok(VERIFY_WRITE, lspOutBuffer, sizeof(lspOutBuffer)))
+	{
+		printk(KERN_ERR "access_ok failed module_access_ok\n");
+
+		if (! access_ok(VERIFY_READ, lspOutBuffer, sizeof(lspOutBuffer)))
+		{
+			printk(KERN_ERR "continue failed read\n");
+		}
+		return -EFAULT;
+	}
+
+	lCurrentTimeKernel = current_kernel_time();
+	getnstimeofday(&lCurrentTimeDay);
+
+	sprintf(lspOutBuffer,
+		"current_kernel_time: %lu %lu\ngetnstimeofday: %lu %lu\n",
+		lCurrentTimeKernel.tv_sec,
+		lCurrentTimeKernel.tv_nsec,
+		lCurrentTimeDay.tv_sec,
+		lCurrentTimeDay.tv_nsec
+	);
+
+	lnCopyToUser = copy_to_user(buffer, lspOutBuffer, strlen(lspOutBuffer)+1);
+
+	vfree(lspOutBuffer);
+	if (lnCopyToUser > 0)
+	{
+		printk(KERN_ERR "copy_to_user failed mytime %d\n", lnCopyToUser);
+        return -EFAULT;
+	}
+
+	return lnCopyToUser;
+}
+
+static struct file_operations my_fops = {
+	.owner = THIS_MODULE,
+	.read = my_read
+};
+
+static struct miscdevice my_time_device = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "mytime",
+	.fops = &my_fops
+};
+
+// called when module is installed
+int __init init_module(void)
+{
+	printk(KERN_INFO "mymodule: Hello %s!\n", name);
+
+	misc_register(&my_time_device);
+
+	return 0;
+}
+
+
+
+// called when module is removed
+void __exit cleanup_module(void)
+{
+	printk(KERN_INFO "mymodule: Goodbye, cruel %s!!\n", name);
+
+	misc_deregister(&my_time_device);
+}
--- linux/demos/module/userprogram.c
+++ linux/demos/module/userprogram.c
@@ -1,18 +1,45 @@
-#include 
+#include <sys/time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define N 1500
+#define N 20
+#define MAX_LENGTH 150
 
 int main()
 {
 	struct timeval gtodTimes[N];
 	char *procClockTimes[N];
 	int i;
+	int fd;
+	int bytes_read;
 
 	fd = open("/dev/mytime", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Opening driver failed!\n");
+		exit(1);
+	}
 
 	for (i = 0; i < N; i++)
 	{
-		printf("...", gtodTimes[i], procClockTime[i]);
+		gettimeofday(&gtodTimes[i], 0);
+
+		procClockTimes[i] = malloc(4000);
+		bytes_read = read(fd, procClockTimes[i], MAX_LENGTH);
+	}
+
+	close(fd);
+
+
+	for (i = 0; i < N; i++)
+	{
+		printf("Library:%ld %ld\nKernel:%s\n",
+			gtodTimes[i].tv_sec,
+			gtodTimes[i].tv_usec,
+			procClockTimes[i]
+		);
 	}
 
 	return 0;
